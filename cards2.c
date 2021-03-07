bd8ad872343af3b30e70f86485c876cf8d5dc760
joystickus <joystickus@yandex.ru>
2021-03-07 14:31:36 +0300

    Initial commit from laptop.

---
+++ cards2.c
@@ -0,0 +1,34 @@
+/*
+ * Program to evaluate face values.
+ * Released uder the Vegas Public license.
+ * (c)2014 The College Blackjack Team.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	char card_name[3];
+	puts("Enter the card_name: ");
+	scanf("%2s", card_name);
+	int val = 0;
+	switch(card_name[0]) {
+	case 'K':
+	case 'Q':
+	case 'J':
+		val = 10;
+		break;
+	case 'A':
+		val = 11;
+		break;
+	default:
+		val = atoi(card_name);
+	}
+	printf("The card value is: %i\n", val);
+	/* Check if the value is 3 to 6 */
+	if ((val > 2) && (val < 7))
+		puts("Count has gone up");
+	/* Otherwise check if the card was 10, J, Q, or K */
+	else if (val == 10)
+		puts("Count has gone down");
+	return 0;
+}
