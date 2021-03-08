0445876a3b4512cb6f8c279367b9b536dffe0d76
joystickus <joystickus@yandex.ru>
2021-03-08 16:54:40 +0300

    08.03.2021 Working on champagne 1-2

---
+++ champagne1.c
@@ -0,0 +1,36 @@
+/*
+ * We'll start with 30 bottles
+ * - that's about 180 glasses
+ * While there's still some fizz:
+ *   Display the current stock
+ *   Enter the number of glasses ordered
+ *   Ajust the stock
+ *   Then just go round again
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int stock = 180;
+	char order_string[3];
+	int order;
+	while(stock > 0) {
+		printf("%i glasses left\nHow many glasses do you need? ", stock);
+		scanf("%2s", order_string);
+		order = atoi(order_string);
+		stock = stock - order;
+		printf("You ordered %i glasses\n", order);
+	}
+	puts("We're out of stock, baby!");
+	return 0;
+}
+
+/*
+ * - order_string
+ * - stock
+ * - "%2s"
+ * - "%i glasses left\n"
+ * - order
+ * - order
+ * - stock
+ */
---
+++ champagne2.c
@@ -0,0 +1,34 @@
+/*
+ * We'll start with 30 bottles
+ * - that's about 180 glasses
+ * While there's still some fizz:
+ *   Display the current stock
+ *   Enter the number of glasses ordered
+ *   Ajust the stock
+ *   Then just go round again
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int stock = 180;
+	int order;
+	while(stock > 0) {
+		printf("%i glasses left\nHow many glasses do you need? ", stock);
+		scanf("%i", &order);
+		stock = stock - order;
+		printf("You ordered %i glasses\n", order);
+	}
+	puts("We're out of stock, baby!");
+	return 0;
+}
+
+/*
+ * - order_string
+ * - stock
+ * - "%2s"
+ * - "%i glasses left\n"
+ * - order
+ * - order
+ * - stock
+ */
