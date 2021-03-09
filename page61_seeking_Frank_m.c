2f331f36da7cca613c879912a650d4612da5bf7e
joystickus <joystickus@yandex.ru>
2021-03-09 22:47:12 +0300

    Working till page 68. Too tired for now...

---
+++ page61_seeking_Frank_m.c
@@ -0,0 +1,27 @@
+/*
+ * Track list:
+ * I left my heart in Harvard med school
+ * Newark, Newark - a wonderful town
+ * Dancing with a Dork
+ * From here to maternity
+ * The girl from Iwo Jima
+ */
+
+/*
+ * Ask the user for the text they're looking for
+ * Loop through all of the track names
+ * If a track name contains the search text then:
+ * Display the track name
+ */
+
+#include <stdio.h>
+#include <string.h>
+int main(){
+	char tracks[][81]{
+		"I left my heart in Harvard med school"
+		"Newark, Newark - a wonderful town"
+		"Dancing with a Dork"
+		"From here to maternity"
+		"The girl from Iwo Jima"
+	}
+}
---
+++ page68_pool_puzzle.c
@@ -0,0 +1,27 @@
+void find_track(char search_for[]){
+	int i;
+	for (i = 0; i < 5; i++){
+		if ( ............ ( ............ , ............ ))
+			printf("Trach %i: '%s'\n", ............ , ............ );
+	}
+}
+
+/*
+ * my
+ * tracks[i]
+ * strstr
+ * search_for
+ * "Sinatra"
+ * way
+ * i
+ * tracks[i]
+ */
+
+my
+tracks[i]
+strstr
+search_for
+"Sinatra"
+way
+i
+tracks[i]
