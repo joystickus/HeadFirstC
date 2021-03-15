e4a16f9428c54f340aaf10a7f7bc92ede1aa9282
joystickus <joystickus@yandex.ru>
2021-03-15 22:19:02 +0300

    finalized chapter 2

--- page87_magnets_answer.c
+++ page87_magnets_answer.c
@@ -1,9 +1,9 @@
 void print_reverse(char* s){
 	size_t len = strlen(s);
-	char* t = ........... + ........... - 1;
-	while ( ........... >= ........... ){
+	char* t = s + len - 1;
+	while (t >= s){
 		printf("%c", *t);
-		t = ........... ........... ........... ;
+		t = t - 1;
 	}
 	puts("");
 }
