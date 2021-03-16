7f28f02e236bd76eb8a054ef9f5b1cc78245bbd1
joystickus <joystickus@yandex.ru>
2021-03-16 22:25:16 +0300

    more json

--- page97_data_to_JSON_m.c
+++ page97_data_to_JSON_m.c
@@ -1,3 +1,19 @@
+/*
+ * data
+ * 42.363400,-71.098465,Speed = 21
+ * 42.363327,-71.097588,Speed = 23
+ * 42.363255,-71.096710,Speed = 17
+ */
+
+/*
+ * JSON
+ * data=[
+ * {latitude: 42.363400, longitude: -71.098465, info: 'Speed = 21'},
+ * {latitude: 42.363327, longitude: -71.097588, info: 'Speed = 23'},
+ * {latitude: 42.363255, longitude: -71.096710, info: 'Speed = 17'},
+ * ]
+ */
+
 #include <stdio.h>
 
 int main(){
