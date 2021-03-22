f34e9ecf042a58c1a27622193fabf1ccac064413
joystickus <joystickus@yandex.ru>
2021-03-22 05:19:10 -0700

    123

--- 3x1.c
+++ 3x1.c
@@ -1,7 +1,38 @@
-/* This is a simple game called "3x+1"
+/* This is a simple game called "3x+1".
+ * The rules are simple -- you enter a number and I will do a series of simple arithmetic actions to it.
+ * If the number is even I'll devide it by 2 and if it's odd I'll multiply it by 3 and add 1.
+ * And I'll do the same with the resulting number. And with the next number. And so on and so forth.
+ * I'll be doing it and showing you the result until I reach the cycle "4-2-1" which is the end of the game.
+ * And then I'll show you how many steps it took to reach the cycle.
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main()
+{
+	char number[5];
+	int inumber;
+	puts("This is a simple game called '3x+1'.");
+	puts("The rules are simple -- you enter a number and I will do a series of simple arithmetic actions to it.");
+	puts("If the number is even I'll devide it by 2 and if it's odd I'll multiply it by 3 and add 1.");
+	puts("And I'll do the same with the resulting number. And with the next number. And so on and so forth.");
+	puts("I'll be doing it and showing you the result until I reach the loop '4-2-1' which is the end of the game.");
+	puts("And then I'll show you how many steps it took to reach the loop.\n");
+	puts("So please enter your number: ");
+	scanf("%5s", number);
+	puts("Let's rock!");
+	inumber = atoi(number);
+	while (inumber != 1) {
+		if (inumber % 2) {
+			inumber = (inumber * 3 + 1);
+			printf("%i\n", inumber);
+		}
+		else {
+			inumber = (inumber / 2);
+			printf("%i\n", inumber);
+		}
+	}
+	puts("That's it! The next number is 4 which takes us to the loop.");
+	return 0;
 }
