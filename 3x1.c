47660137b658ac1bc1c35e21bd38e827fe9f07b6
joystickus <joystickus@yandex.ru>
2021-03-22 19:22:03 +0300

    adjusted tenses a bit

--- 3x1.c
+++ 3x1.c
@@ -16,11 +16,11 @@ int main()
 	int inumber;
 	int count = 0;
 	puts("This is a simple game called '3x+1'.");
-	puts("The rules are simple -- you enter a number and I will do a series of simple arithmetic actions to it.");
-	puts("If the number is even I'll devide it by 2 and if it's odd I'll multiply it by 3 and add 1.");
-	puts("And I'll do the same with the resulting number. And with the next number. And so on and so forth.");
-	puts("I'll be doing it and showing you the result until I reach the loop '4-2-1' which is the end of the game.");
-	puts("And then I'll show you how many steps it took to reach the loop.\n");
+	puts("The rules are simple -- you enter a number and I do a series of simple arithmetic actions to it.");
+	puts("If the number is even I devide it by 2 and if it's odd I multiply it by 3 and add 1.");
+	puts("And I do the same with the resulting number. And with the next number. And so on and so forth.");
+	puts("I'm doing it and showing you the result until I reach the loop '4-2-1' which is the end of the game.");
+	puts("And then I show you how many steps it took me to reach the loop.\n");
 	puts("So please enter your number: ");
 	scanf("%5s", number);
 	inumber = atoi(number);
