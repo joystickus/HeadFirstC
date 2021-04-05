501c58b38e3bab3f98dbd383cd4630eec27981ae
joystickus <joystickus@yandex.ru>
2021-04-05 05:04:32 -0700

    Started answering page3 task.

---
+++ page3_Sharpen_your_pencil.c
@@ -0,0 +1,12 @@
+int card_count = 11;					/* Create an integer variable and set it to 11. */
+if (card_count > 10)					/* Check if the value of the variable is more than 10. */
+	puts("The deck is hot. Increase bet.");		/* If so, print an unformatted message. */
+
+int c = 10;						/* Create an integer variable and set it to 10. */
+while (c > 0) {						/* Create a cycle for checking if the value of the variable is more than 0. */
+	puts("I must not write code in class");		/* While the value of the variable is more than 0, print an unformatted message. */
+	c = c - 1;					/* Decrease the value of the variable by 1. */
+}							/* End of the embedded condition */
+
+/* Assume name shorter than 20 chars. */		/* Comment. */
+char ex[20];						/* 
