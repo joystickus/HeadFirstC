eae3368cdc7d7e45c7f27f90dbd1c3181f323797
joystickus <nasyrovdp@yandex.ru>
2021-04-12 05:28:18 -0700

    p23

--- p23.c
+++ p23.c
@@ -5,39 +5,13 @@ Your job is to play compiler and determine whether each of these files
 will compile, and if not, why not.
 For extra bonus points, say what you think the output of each compiled
 file will be when run, and whether you think the code is working as intended.
-*/
+ */
 
-/* p23A -- It will not compile as the second 'if' is not well intended. */
-#include <stdio.h>
-int main()
-{
-	int card = 1;
-	if (card > 1) /* There is no opening curly brace here. */
-		card = card - 1;
-		if (card < 7)
-			puts("Small card"); /* There is no closing curly brace here. */
-	else { /* This curly brace is not needed here. */
-		puts("Ace!");
-	} /* This curly brace is not needed here. */
-	return 0;
-}
-
-/* p23B -- It will not compile as the closing curly brace is not well placed. */
-#include <stdio.h>
-int main()
-{
-	int card = 1;
-	if (card > 1) {
-		card = card - 1;
-		if (card < 7)
-			puts("Small card");
-	else
-		puts("Ace!");
-	} /* This curly brace should be two lines above, before 'else'. */
-	return 0;
-}
-
-/* p23C -- */
+/* p23A
+ * It will compile.
+ * It'll give "Small card".
+ * It doesn't work as intended as 'else' is related to the wrong 'if'.
+ */
 #include <stdio.h>
 int main()
 {
@@ -52,7 +26,47 @@ int main()
 	return 0;
 }
 
-/* p23D -- */
+/* p23B
+ * It will compile.
+ * It'll give nothing.
+ * It doesn't work as intended as 'else' goes within the set of action of the first 'if'.
+ */
+#include <stdio.h>
+int main()
+{
+	int card = 1;
+	if (card > 1) {
+		card = card - 1;
+		if (card < 7)
+			puts("Small card");
+	else
+		puts("Ace!");
+	}
+	return 0;
+}
+
+/* p23C
+ * It will compile.
+ * It'll give "Small card".
+ * It works as intended.
+ */
+#include <stdio.h>
+int main()
+{
+	int card = 1;
+	if (card > 1)
+		card = card - 1;
+		if (card < 7)
+			puts("Small card");
+	else {
+		puts("Ace!");
+	}
+	return 0;
+}
+
+/* p23D
+ * It will not compile.
+ */
 #include <stdio.h>
 int main()
 {
