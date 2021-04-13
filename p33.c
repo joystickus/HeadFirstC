3568b80148668a0abca3232323942d09078a994d
joystickus <nasyrovdp@yandex.ru>
2021-04-13 07:59:28 -0700

    p33

--- p33.c
+++ p33.c
@@ -9,30 +9,43 @@
 int main()
 {
 	char card_name[3];
-	puts("Enter the card_name: ");
-	scanf("%2s", card_name);
-	int val = 0;
 	int count = 0;
-	switch(card_name[0]) {
-		case 'K':
-		case 'Q':
-		case 'J':
-			val = 10;
-			break;
-		case 'A':
-			val = 11;
-			break;
-		default:
-			val = atoi(card_name);
-	}
-	printf("The card value is: %i\n", val);
-	/* Check if the value is 3 to 6 */
-	if ((val >= 3) && (val <= 6)) {
-		count++;
-		printf("Count has gone up and is now %i\n", count);
-	} else if (val == 10) {
-		count--;
-		printf("Count has gone down and is now %i\n", count);
+	while (card_name[0] != 'X') {
+		puts("Enter the card name (X to exit): ");
+		scanf("%2s", card_name);
+		int val = 0;
+		switch(card_name[0]) {
+			case 'K':
+			case 'Q':
+			case 'J':
+				val = 10;
+				printf("The card value is %i\n", val);
+				break;
+			case 'A':
+				val = 11;
+				printf("The card value is %i\n", val);
+				break;
+			case 'X':
+				puts("That's it. Thank you!");
+				printf("Current count is %i\n", count);
+				continue;
+			default:
+				val = atoi(card_name);
+				if ((val < 2) || (val > 10)) {
+					puts("This is not a valid card name. Try again.");
+					printf("Current count is %i\n\n", count);
+					continue;
+				}
+				printf("The card value is %i\n", val);
+		}
+		if ((val >= 3) && (val <= 6)) {
+			count++;
+			puts("Count has gone up!");
+		} else if (val == 10) {
+			count--;
+			puts("Count has gone down!");
+		}
+		printf("Current count is %i\n\n", count);
 	}
 	return 0;
 }
