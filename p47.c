1b788d324dd4195bf38b5b085d481b676b1d65e9
joystickus <joystickus@yandex.ru>
2021-04-14 17:52:34 +0300

    p47

--- p47.c
+++ p47.c
@@ -3,12 +3,10 @@
 int main()
 {
 	int stock = 180;
-	char order_string[4];
 	int order;
 	while(stock > 0) {
 		printf("%i glasses left\nHow many glasses do they want? ", stock);
-		scanf("%3s", order_string);
-		order = atoi(order_string);
+		scanf("%i", &order);
 		stock = stock - order;
 		printf("You ordered %i glasses\n", order);
 	}
