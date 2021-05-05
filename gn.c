545d89ea62e6a16414041dc3247ca29c3c22bd68
joystickus <nasyrovdp@yandex.ru>
2021-05-05 07:30:58 -0700

    gn

--- gn.c
+++ gn.c
@@ -11,36 +11,26 @@ int main()
 	puts("This is a simple game called 'Guess a number'.");
 	puts("The rules are simple -- I think of a number from 1 to 100 and you have to guess it in 7 tries.");
 	puts("So I've come up with a number. Now it's your turn.");
-	int number = rand() % 101;
+	int number = rand() % 100 + 1;
 	int guess;
 	int count = 7;
-	while (count != 0) {
-		scanf("%i", guess);
-		if (guess = number) {
-			printf("You've guessed it! The number is %i!\n", number);
-			break;
-		} else if (guess < number) {
-			if (count != 2) {
-				count--;
-				printf("My number is bigger than that! You have %i tries left.\n", count);
-				continue;
-			} else {
-				count--;
-				printf("My number is bigger than that! You have %i try left.\n", count);
-				continue;
-			}
+	while (count > 0) {
+		scanf("%i", &guess);
+		printf("Your guess is %i\n", guess);
+		if (guess < number) {
+			count--;
+			printf("My number is bigger. Tries left: %i.\n", count);
+			continue;
 		} else if (guess > number) {
-			if (count != 2) {
-				count--;
-				printf("My number is smaller than that! You have %i tries left.\n", count);
-				continue;
-			} else {
-				count--;
-				printf("My number is smaller than that! You have %i try left.\n", count);
-				continue;
-			}
+			count--;
+			printf("My number is smaller. Tries left: %i.\n", count);
+			continue;
+		} else {
+			puts("You win!");
+			break;
 		}
-		printf("Alas! You lose! The number was %i.\n", number);
 	}
+	puts("The end!");
+	printf("My number is %i\n", number);
 	return 0;
 }
